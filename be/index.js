d586302a91a062442ac89ae16a470f0805bf728f
ShivaniSinha <shivanisinha24dec@gmail.com>
2023-09-26 22:17:28 +0530

    index.js and vercel.json updated to previous

--- be/index.js
+++ be/index.js
@@ -1,18 +1,19 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from 'dotenv'
 dotenv.config();
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import fileUpload from 'express-fileupload';
 import * as path from 'path'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+
 import userRoutes from './routes/user.js';
 import postRoutes from './routes/post.js';
 import conversationRoutes from './routes/conversation.js';
 import messagesRoutes from './routes/messages.js';
-import fileUpload from 'express-fileupload';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -24,8 +25,9 @@ app.use(fileUpload())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
 // app.use("/uploads", express.static("./uploads"));  // upload folder should be in is in root directory of backed folder
-app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // upload folder should be in is in root directory of backed folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads"))); 
 
 const conf = () => {
   //mongoose.connect('mongodb://localhost/admissionjockey');
@@ -69,8 +71,7 @@ app.use("/message",messagesRoutes);
 
 // const HOST = 'https://trial2-api.vercel.app';
 const PORT = process.env.PORT || 3009;
-
 // httpServer.listen(PORT, HOST, () => {
-httpServer.listen(PORT, () => {
-  console.log(`Server started on :${PORT}`);
+httpServer.listen(3009, () => {
+  console.log(`Server started on :3009`);
 });
