83a06b392355c04bbcd6c6319c5291137ce63421
ShivaniSinha <shivanisinha24dec@gmail.com>
2023-09-26 15:50:36 +0530

    update index.js in be

--- be/index.js
+++ be/index.js
@@ -10,106 +10,60 @@ import userRoutes from './routes/user.js';
 import postRoutes from './routes/post.js';
 import conversationRoutes from './routes/conversation.js';
 import messagesRoutes from './routes/messages.js';
-import * as path from 'path'
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import fileUpload from 'express-fileupload';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const absolutePath = path.resolve(__dirname, "..", "uploads");
-
-mongoose.set('strictQuery', false);
 const app = express();
-app.use(fileUpload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-})
-
 // app.use("/uploads", express.static("./uploads"));  // upload folder should be in is in root directory of backed folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // upload folder should be in is in root directory of backed folder
+mongoose.set('strictQuery', false);
 
-const conf = () => {
-    
-  //mongoose.connect('mongodb://localhost/admissionjockey');
-  mongoose.connect(process.env.MONGOOSE_CONNECTION_LINK);
-  // mongoose.connect('mongodb://localhost/edudb');
-  const db= mongoose.connection;
-  db.on('error',console.error.bind('Unable to connect to the database'));
-  db.once("open",function calback(){
-      console.log("Connection has been established successfully!!");
-   
-  })
+
+// main().catch((err) => console.log(err));
+try {
+  await mongoose.connect(process.env.MONGOOSE_CONNECTION_LINK, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("Connected to MongoDB Atlas");
+} catch (error) {
+  console.error("Error connecting to MongoDB Atlas:", error);
 }
-conf()
+
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
-    // origin: "https://facebook-clone-gray-three.vercel.app",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE","HEAD"],
+    origin: "https://facebook-colne-theta.vercel.app",
+    methods: ["GET", "POST","PUT", "PATCH", "DELETE"],
     preflightContinue:true,
     credentials:true,
     optionsSuccessStatus: 204
-  },
-  // allowEIO3:true,
+  }
 });
 
 io.on("connection", (socket) => {
-  socket.on("joinRoom", (room) => {
-    socket.join(room);
-  });
 
-  socket.on("newMessage", ({ newMsgObj, room }) => {
-    if (newMsgObj) {
-      // Log the received payload for debugging
-      // console.log("Received new message:", newMsgObj);
+  socket.on("joinRoom",room => {
+		socket.join(room)
+  })
 
-      // Emit the message to the room
-      io.in(room).emit("getLatestMessage", newMsgObj);
-      
-      // You can also save the message to your database here if needed
-      // For example, if you have a MongoDB model for messages, you can save it.
-    } else {
-      // Handle invalid payload
-      console.error("Invalid message payload:", newMsgObj);
-      // You can also send an error message to the client if needed
-      socket.emit("error", "Invalid message payload");
-    }
-  });
+  socket.on("newMessage", ({newMsgObj, room}) => {
+    io.in(room).emit("getLatestMessage", newMsgObj)
+  })
 
-  // Add a generic error handler for the socket
-  socket.on("error", (error) => {
-    console.error("Socket error:", error);
-  });
 });
-;
-
-// io.on("connection", (socket) => {
-
-//   socket.on("joinRoom",room => {
-// 		socket.join(room)
-//   })
-
-//   socket.on("newMessage", ({newMsgObj, room}) => {
-//     io.in(room).emit("getLatestMessage", newMsgObj)
-//   })
-
-// });
-
+app.use("/home",(req,res)=>{res.send("Hello")})
 app.use("/user",userRoutes);
 app.use("/post",postRoutes);
 app.use("/conversation",conversationRoutes);
 app.use("/message",messagesRoutes);
 
-httpServer.listen(3009, () => {
-  console.log("server started on port :3009");
+// const HOST = 'https://trial2-api.vercel.app';
+const PORT = process.env.PORT || 3009;
+
+// httpServer.listen(PORT, HOST, () => {
+httpServer.listen(PORT, () => {
+  console.log(`Server started on :${PORT}`);
 });
